6451d34fcf613542838c1642d375ef9930d81e1e
Marinko <marinkospasojevic@gmail.com>
2019-02-18 20:48:47 +0100

    Added new .NET Core version 2.2

---
+++ AppEnd/ActionFilters/ActionFilters/Controllers/MovieController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using ActionFilters.ActionFilters;
+using ActionFilters.Entities;
+using ActionFilters.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionFilters.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly MovieContext _context;
+
+        public MovieController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var movies = _context.Movies.ToList();
+
+            return Ok(movies);
+        }
+
+        [HttpGet("{id}", Name = "MovieById")]
+        [ServiceFilter(typeof(ValidateEntityExistsAttribute<Movie>))]
+        public IActionResult Get(Guid id)
+        {
+            var dbMovie = HttpContext.Items["entity"] as Movie;
+
+            return Ok(dbMovie);
+        }
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public IActionResult Post([FromBody] Movie movie)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("MovieById", new { id = movie.Id }, movie);
+        }
+
+        [HttpPut("{id}")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        [ServiceFilter(typeof(ValidateEntityExistsAttribute<Movie>))]
+        public IActionResult Put(Guid id, [FromBody]Movie movie)
+        {
+            var dbMovie = HttpContext.Items["entity"] as Movie;
+
+            dbMovie.Map(movie);
+
+            _context.Movies.Update(dbMovie);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ServiceFilter(typeof(ValidateEntityExistsAttribute<Movie>))]
+        public IActionResult Delete(Guid id)
+        {
+            var dbMovie = HttpContext.Items["entity"] as Movie;
+
+            _context.Movies.Remove(dbMovie);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ AppEnd/ActionFilters/ActionFilters/Entities/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionFilters.Entities
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
