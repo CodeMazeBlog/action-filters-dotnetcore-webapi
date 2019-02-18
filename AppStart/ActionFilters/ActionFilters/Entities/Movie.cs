53b85bf58485688e1eab73a498c865bc16c2ac26
Marinko <marinkospasojevic@gmail.com>
2019-02-18 20:26:09 +0100

    .NET Core 2.2 change

---
+++ AppStart/ActionFilters/ActionFilters/Entities/Movie.cs
@@ -0,0 +1,20 @@
+﻿using ActionFilters.Contracts;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ActionFilters.Entities
+{
+    [Table("Movie")]
+    public class Movie: IEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Genre is required")]
+        public string Genre { get; set; }
+        [Required(ErrorMessage = "Director is required")]
+        public string Director { get; set; }
+    }
+}
